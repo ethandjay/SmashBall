d2f014af37328637bc72e554d3453c042d4e9f22
Vu Ngo <m.ngo@wustl.edu>
2017-11-15 12:33:24 -0600

    first commit

--- SmashBall/ViewController.swift
+++ SmashBall/ViewController.swift
@@ -39,7 +39,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Run the view's session
         sceneView.session.run(configuration)
         
-        addObject()
+        Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(addObject), userInfo: nil, repeats: true)
+        
+        print("test")
+        
+        
     }
     
     @objc func addObject(){
@@ -47,7 +51,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         ship.loadModal()*/
         
         let xPos = randomPosition(lowerBound: -10.0 , upperBound: 10.0)
-        let yPos = randomPosition(lowerBound: -1.5, upperBound: 1.5)
+        let yPos = randomPosition(lowerBound: -1.0, upperBound: 1.0)
         let zPos = randomPosition(lowerBound: -6.0, upperBound: 6.0)
         
         let position = SCNVector3Make(xPos, yPos, zPos)
@@ -63,14 +67,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         sceneView.scene.rootNode.addChildNode(node)
         
-        let randSpeed = SCNVector3(-xPos/10, -yPos/10, -zPos/10)
+        let randSpeed = SCNVector3(-xPos/5, -yPos/5, -zPos/5)
         node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
         node.physicsBody?.isAffectedByGravity = false
         node.physicsBody?.applyForce(randSpeed, asImpulse: true)
+    
         
-        Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(addObject), userInfo: nil, repeats: false)
     }
     
+    /*func distanceIsValid (xPos:Float,) -> Bool {
+        
+    }*/
     
     
     func randomPosition (lowerBound lower:Float, upperBound upper:Float) -> Float {
@@ -79,18 +86,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        print("Test")
-        
+ 
         if let touch = touches.first {
             let location = touch.location(in: sceneView)
             
             let hitList = sceneView.hitTest(location, options: nil)
-            print("test2")
+          
             if let hitObject = hitList.first {
                 let node = hitObject.node
-                print("test3")
+             
                 if node.name == "ball" {
-                    print("test4")
+                
                     node.removeFromParentNode()
                 }
             }
